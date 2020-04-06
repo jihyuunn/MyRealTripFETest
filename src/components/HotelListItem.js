7d589f415356b52e22a1a67626e74952692f71dd
jihyuunn <791004_@likelion.org>
2020-04-06 14:17:16 +0900

    📥 price list hanlder / loading spinner

--- src/components/HotelListItem.js
+++ src/components/HotelListItem.js
@@ -11,13 +11,14 @@ const ItemContainer = styled.div`
     }
 `
 
-const HotelListItem = ({hotel}) => {
+const HotelListItem = ({hotel, prices}) => {
     return (
         <ItemContainer>
-            <img src={hotel.imageUrl} alt='hotel' />
+            <img src={hotel.imageUrl} alt='hotel' onClick={() => console.log(prices)}/>
             <div>
                 <div>{hotel.freeServices}</div>
                 <h2>{hotel.name}</h2>
+                <p>{prices[hotel.id] ? prices[hotel.id][hotel.id] : 'loading'}</p>
                 <div>
                     <p><span role="img">⭐️</span> {hotel.rate}</p>
                     <p>리뷰점수 {hotel.rate}</p>
--- src/pages/HotelList.js
+++ src/pages/HotelList.js
@@ -26,6 +26,44 @@ const ListContainer = styled.div`
     }
   }
 `
+const LoadingSpinner = styled.div`
+.lds-facebook {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-facebook div {
+  display: inline-block;
+  position: absolute;
+  left: 8px;
+  width: 16px;
+  background: gray;
+  animation: lds-facebook 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+}
+.lds-facebook div:nth-child(1) {
+  left: 8px;
+  animation-delay: -0.24s;
+}
+.lds-facebook div:nth-child(2) {
+  left: 32px;
+  animation-delay: -0.12s;
+}
+.lds-facebook div:nth-child(3) {
+  left: 56px;
+  animation-delay: 0;
+}
+@keyframes lds-facebook {
+  0% {
+    top: 8px;
+    height: 64px;
+  }
+  50%, 100% {
+    top: 24px;
+    height: 32px;
+  }
+}
+`
 
 const HotelList = () => {
     const [range, setRange] = useState({
@@ -42,7 +80,11 @@ const HotelList = () => {
 
     const [hotels, setHotels] = useState([])
     const [page, setPage] = useState(1)
+    const [isLoading, setIsLoading] = useState(false)
+    const [isLoadingFail, setIsLoadingFail] = useState(false)
     const getHotelList = async() => {
+        setIsLoading(true)
+        setIsLoadingFail(false)
         try {
             const response = await Axios.get(
                 `${process.env.REACT_APP_BASE_API}/hotels?page=${page}`
@@ -51,15 +93,37 @@ const HotelList = () => {
             setHotels(response.data)
         } catch (e) {
             console.log(e)
+            setIsLoadingFail(true)
+        }
+        setIsLoading(false)
+    }
+    const [hotelPrices, setHotelPrices] = useState([])
+    const getHotelPrice = async() => {
+        for (let i=0; i<20; i++) {
+            try {
+                const response = await Axios.get(
+                    `${process.env.REACT_APP_BASE_API}/hotel-prices?ids=${i}`
+                )
+                console.log(response.data[i])
+                setHotelPrices(oldArray => [...oldArray, response.data])
+            } catch (error) {
+                console.log(error)
+            }
         }
     }
     useEffect(() => {
-        getHotelList();
+        const getItems = async() => {
+            await getHotelList();
+            await getHotelPrice();
+        }
+        getItems();
     }, [])
     const List = () => {
-        return (
+        return (isLoading ? <LoadingSpinner>
+            <div className="lds-facebook"><div></div><div></div><div></div></div>
+            </LoadingSpinner>:
             hotels.map((hotel,index) => (
-                <HotelListItem hotel={hotel} key={index}/>
+                <HotelListItem hotel={hotel} key={index} prices={hotelPrices}/>
             ))
         )
     }
@@ -69,7 +133,7 @@ const HotelList = () => {
                 <InputRage maxValue={1000000} minValue={0} value={range} onChange={rangeHandler} />
             </div>
             <div className="list">
-                {List()}
+                {isLoadingFail ? <div>다시 시도해주세요<button onClick={getHotelList}>retry</button></div>:List()}
             </div>
         </ListContainer>
     )
